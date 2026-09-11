7774ae271aba4d5162b2d7f597648fc6ec80db9f
Jesutoni Odufeso <ojesutoni@gmail.com>
2026-09-11 21:03:57 +0100

    feat: enhance scoreboard functionality with scoring logic and styling updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,37 @@
-console.log('Happy developing ✨')
+let homeEl = document.getElementById("home-el");
+let awayEl = document.getElementById("away-el");
+
+let scoreHome = 0
+let scoreAway = 0
+
+// Home Scores
+function add1home() {
+    scoreHome += 1;
+    homeEl.textContent = scoreHome;
+}
+
+function add2home() {
+    scoreHome += 2;
+    homeEl.textContent = scoreHome;
+}
+
+function add3home() {
+    scoreHome += 3;
+    homeEl.textContent = scoreHome;
+}
+
+// Home Scores
+function add1away() {
+    scoreAway += 1;
+    awayEl.textContent = scoreAway;
+}
+
+function add2away() {
+    scoreAway += 2;
+    awayEl.textContent = scoreAway;
+}
+
+function add3away() {
+    scoreAway += 3;
+    awayEl.textContent = scoreAway;
+}
